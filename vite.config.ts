3d05d3be52228b74b1c31af5b27c8ca1a9f02d1b
codearslan <hasan.arslan@gmail.com>
2025-12-03 23:30:59 -0500

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/insightbox/',
+  base: process.env.VERCEL ? '/' : '/insightbox/',
   build: {
     outDir: 'dist',
   },
